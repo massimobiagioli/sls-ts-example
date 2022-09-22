78c8f2e13cda4f388772af7774afa7f5dc6d7088
Massimo Biagioli <massimo.biagioli@flowing.it>
2022-09-22 15:35:19 +0200

    Change hello handler

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -6,7 +6,7 @@ import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the version 1.0`,
+    message: `Hello ${event.body.name}, welcome to the version 1.1`,
   });
 }
 
